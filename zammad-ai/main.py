ce28e0a23b8b2f2d797fd418d8d95d3c0888d745
Fabian Reinold <fabian.reinold@muenchen.de>
2025-11-07 10:53:33 +0100

    feat(main): add inital app start

--- zammad-ai/main.py
+++ zammad-ai/main.py
@@ -1,11 +1,20 @@
+# ruff: noqa: E402
+from dotenv import load_dotenv
 from truststore import inject_into_ssl
 
+load_dotenv()
 inject_into_ssl()
 
+import asyncio
 
-def main():
-    print("Hello from zammad-ai!")
+from src.kafka import app
+from src.settings import Settings, get_settings
+
+
+async def main() -> None:
+    _: Settings = get_settings()
+    await app.run()  # blocking method
 
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
