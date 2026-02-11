9c0b5eaecf02847f18ccc667270cd104c798f08d
Fabian Reinold <fabian.reinold@muenchen.de>
2026-02-11 14:45:48 +0100

    refactor(main): remove argparse and streamline backend startup logic

--- zammad-ai/main.py
+++ zammad-ai/main.py
@@ -5,18 +5,27 @@ from truststore import inject_into_ssl
 load_dotenv()
 inject_into_ssl()
 
-import argparse
+
+from logging import Logger
 
 import uvicorn
 
+from app.api.backend import backend
+from app.core.settings import ZammadAISettings, get_settings
 from app.utils.logging import getLogger
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-d", "--development", action="store_true")
-    args = parser.parse_args()
+    logger: Logger = getLogger()
+    settings: ZammadAISettings = get_settings()
+    logger.info(msg="Starting Zammad AI Backend")
 
-    logger = getLogger()
-    logger.info("Starting Zammad AI Backend")
+    if settings.mode == "unittest":
+        logger.error(msg="Zammad AI cannot be started in unittest mode.")
+        exit(code=1)
 
-    uvicorn.run("app.api.backend:backend", host="localhost", port=8080, log_config="logconf.yaml")
+    hosts: dict[str, str] = {
+        "development": "localhost",
+        "production": "0.0.0.0",
+    }
+
+    uvicorn.run(app=backend, host=hosts[settings.mode], port=8080, log_config="logconf.yaml")
